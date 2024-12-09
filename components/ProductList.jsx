d902445b031c63cc26672e7f055c668884704e89
orgi-kjjung <thek226@gmail.com>
2024-12-09 10:56:18 +0900

    feat: add component(product)

---
+++ components/ProductList.jsx
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import styles from './ProductList.module.css';
+
+function ProductList() {
+	const [products, setProducts] = useState();
+
+	useEffect(() => {
+		axios.get('http://localhost:4000/products').then(response => {
+			response.data;
+			setProducts(response.data);
+		});
+	}, []);
+
+	console.log(products);
+
+	return (
+		<ul>
+			{products &&
+				products.map(product => {
+					return (
+						<li key={product.id} className={styles.item}>
+							<div>
+								<Image
+									src={product.imageUrl}
+									width={300}
+									height={250}
+									alt={product.name}
+								></Image>
+							</div>
+							<div>{product.name}</div>
+						</li>
+					);
+				})}
+		</ul>
+	);
+}
+
+export default ProductList;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import ProductList from '@/components/ProductList';
 
 // '/'에 해당하는 page component
 function ProductPage() {
-	return <div style={{ color: 'blue' }}>hello next</div>;
+	return (
+		<div>
+			<h1>상품목록 페이지</h1>
+			<ProductList></ProductList>
+		</div>
+	);
 }
 
 export default ProductPage;
