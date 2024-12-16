2e846f729b09ef0b4a705173a96e5fb0ac7b041f
orgi-kjjung <thek226@gmail.com>
2024-12-17 08:19:35 +0900

    fix: product detail

--- pages/products/[productId].jsx
+++ pages/products/[productId].jsx
@@ -1,5 +1,13 @@
+import ProductHeader from '@/components/ProductHeader';
+
 function ProductDetailPage({ message }) {
-	return <div>ProductDetailPage : {message}</div>;
+	const headerTitle = '상품상세 페이지';
+	return (
+		<div>
+			<ProductHeader title={headerTitle} />
+			<div>ProductDetailPage : {message}</div>
+		</div>
+	);
 }
 
 export default ProductDetailPage;
