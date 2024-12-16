60e5fbf9c6f2deb1094536d5290f4951ad6f1621
orgi-kjjung <thek226@gmail.com>
2024-12-17 08:31:43 +0900

    fix: getServerSideProps()

--- pages/products/[productId].jsx
+++ pages/products/[productId].jsx
@@ -1,11 +1,13 @@
 import ProductHeader from '@/components/ProductHeader';
+import axios from 'axios';
 
-function ProductDetailPage({ message }) {
+function ProductDetailPage({ message, productInfo }) {
 	const headerTitle = '상품상세 페이지';
 	return (
 		<div>
 			<ProductHeader title={headerTitle} />
 			<div>ProductDetailPage : {message}</div>
+			<p>{productInfo.name}</p>
 		</div>
 	);
 }
@@ -14,7 +16,12 @@ export default ProductDetailPage;
 
 export async function getServerSideProps(context) {
 	console.log('## productId : ', context.params.productId);
+	const id = context.params.productId;
+	const response = await axios.get(`http://localhost:4000/products/${id}`);
 	return {
-		props: { message: 'send server message' },
+		props: {
+			message: 'send server message',
+			productInfo: response.data,
+		},
 	};
 }
