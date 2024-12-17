b86a4ae001c29920f716b2f82630645db0f0dc29
orgi-kjjung <thek226@gmail.com>
2024-12-18 07:21:23 +0900

    fix: update removeCart

--- components/cart/CartList.jsx
+++ components/cart/CartList.jsx
@@ -1,15 +1,23 @@
 import Image from 'next/image';
 import styles from './CartList.module.css';
-import { removeCartItem } from '@/api';
+import { useRouter } from 'next/router';
+import axios from 'axios';
 
 function CartList({ carts }) {
+	const router = useRouter();
+
 	const totalPrice = carts.reduce((acc, cur) => {
 		return (acc += parseFloat(cur.price));
 	}, 0);
 
 	const removeCart = async id => {
-		await removeCartItem(id);
-		alert('삭제가 되었습니다.');
+		const { data } = await axios.post('http://localhost:3000/api/carts', {
+			data: {
+				id,
+			},
+		});
+		alert(`${data.name}이 삭제 되었습니다`);
+		router.replace(router.asPath);
 	};
 
 	return (
